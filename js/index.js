a88cfce128110253c61893f59d8c13d0e0681ab8
xande1111 <xandedbh@outlook.com>
2022-11-16 20:45:40 -0300

    modal to-do

--- js/index.js
+++ js/index.js
@@ -3,16 +3,16 @@ const eventosMuralIndex = document.querySelector('#eventosIndex')
 const URL = 'https://xp41-soundgarden-api.herokuapp.com/events'
 
 async function mostrarEventos() {
-    try {
-        const response = await fetch(URL)
-        const data = await response.json()
-        const primeirosEventos = data.slice(0, 3)
+  try {
+    const response = await fetch(URL)
+    const data = await response.json()
+    const primeirosEventos = data.slice(0, 3)
 
-        primeirosEventos.forEach(evento => {
-            // formato local 
-            let dataEvento = new Date(evento.scheduled).toLocaleString()
+    primeirosEventos.forEach(evento => {
+      // formato local
+      let dataEvento = new Date(evento.scheduled).toLocaleString()
 
-            let html = `
+      let html = `
       <article class="evento card p-5 m-3">
         <h2>${evento.name} - ${dataEvento}</h2>
         <h4>${evento.attractions}</h4>
@@ -23,14 +23,79 @@ async function mostrarEventos() {
       </article>
       `
 
-            eventosMuralIndex.innerHTML += html
-        })
+      eventosMuralIndex.innerHTML += html
+    })
 
-        console.log(data.slice(0, 3))
-    } catch (error) {
-        console.log(error)
-    }
+    console.log(data.slice(0, 3))
+  } catch (error) {
+    console.log(error)
+  }
 }
-// listar eventos DOM
+// listar eventos dom
 mostrarEventos()
 
+// MODAL
+
+// redirecionar para pagina eventos
+function redirecionar() {
+  window.location.href = 'index.html'
+}
+
+const modal = document.querySelector('#telaModalIndex')
+
+function abrirModalIndex() {
+  event.preventDefault()
+  modal.style.display = 'block'
+  modal.setAttribute('id_evento', event.target.id)
+}
+
+// reserva ingresso onsubmit
+
+const form = document.querySelector('#telaModalIndex form')
+form.addEventListener('submit', fazerReservaIngresso)
+
+async function fazerReservaIngresso() {
+  event.preventDefault()
+  const nome = document.getElementById('nome').value
+  const email = document.getElementById('email').value
+  const ingressos = document.getElementById('qtdIngresso').value
+  const id = modal.getAttribute('id_evento')
+
+  const URL_RESERVA = 'https://xp41-soundgarden-api.herokuapp.com/bookings'
+
+  const reserva = {
+    owner_name: nome,
+    owner_email: email,
+    number_tickets: ingressos,
+    event_id: id
+  }
+
+  try {
+    const response = await fetch(URL_RESERVA, {
+      method: 'POST',
+      body: JSON.stringify(reserva),
+      headers: { 'Content-type': 'application/json' }
+    })
+
+    if (response.ok) {
+      alert('reserva efetuada')
+      redirecionar()
+    } else {
+      console.log(response)
+      throw new Error(`${response.status}`)
+    }
+
+    const result = await response.json()
+    return result
+  } catch (err) {
+    if (err.message === '400') alert('deu b.o pnc')
+    console.log(err)
+  }
+}
+
+// fecha o modal ao clicar
+
+const closeBtn = document.querySelector('#closeBtn')
+closeBtn.onclick = function () {
+  modal.style.display = 'none'
+}

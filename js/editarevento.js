16f0b11ad24ce47a5a4b6557139c60a5fb0d0315
gattihub <flavi503@hotmail.com>
2022-11-17 19:12:17 -0300

    Finalização das funcionalidade JS das páginas editar-evento.html e cadastro-evento.html

--- js/editarevento.js
+++ js/editarevento.js
@@ -7,8 +7,11 @@ const inputDescricao = document.querySelector('#descricao');
 const inputData = document.querySelector('#data');
 const inputLotacao = document.querySelector('#lotacao')
 
-const editarEvento = async () => {
-    const resposta = await fetch (`https://xp41-soundgarden-api.herokuapp.com/events,)
+const id = location.search.substring(4);
+console.log(id);
+
+const localizaURL = async () => {
+    const resposta = await fetch (`https://xp41-soundgarden-api.herokuapp.com/events/${id}`) 
     const dadosEvento = await resposta.json();
 
     inputNome.value = dadosEvento.name;
@@ -19,4 +22,43 @@ const editarEvento = async () => {
     inputLotacao.value = dadosEvento.number_tickets;
 }
 
-editarEvento();
+localizaURL();
+
+
+
+const btnEnviar = document.querySelector('#enviar')
+
+const editarEvento = async (evento) => {
+
+    evento.preventDefault();
+    const EventName = document.querySelector("#nome");
+    const EventPoster= document.querySelector("#banner");
+    const EventAttractions = document.querySelector('#atracoes')
+    const EventDescription = document.querySelector('#descricao')
+    const EventScheduled = document.querySelector('#data')
+    const EventNumber_tickets = document.querySelector('#lotacao')
+
+    const eventObject = {
+        "name": EventName.value,
+        "poster": EventPoster.value ,
+        "attractions":EventAttractions.value.split(",") ,
+        "description":EventDescription.value ,
+        "scheduled": new Date (EventScheduled.value) ,
+        "number_tickets": Number(EventNumber_tickets.value)
+    }
+ 
+    const init = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventObject),
+        redirect: 'follow'
+    }
+    
+    const resposta = await fetch(`https://xp41-soundgarden-api.herokuapp.com/events/${id}`, init )
+    const dados = resposta.json()
+        
+}
+
+btnEnviar.onclick = editarEvento;
